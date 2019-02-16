6c4b2895deb8231ff4f12584f2d94c6418a395ad
Mhammad1704 <natgeo1416@hotmail.com>
2019-02-16 23:50:07 +0300

    Basic task work

--- src/App.js
+++ src/App.js
@@ -3,27 +3,59 @@ import logo from './logo.svg';
 import './App.css';
 import Todo from './todo.js'
 class App extends Component {
-  render() {
-    const list = [
-      {
-        "title": "go out",
-        "text" : " buy stuff"
-      },
-      {
-        "title": "go out",
-        "text" : " buy stuff"
-      },
-      {
-        "title": "go out",
-        "text" : " buy stuff"
-      }
-    ]
 
-    const todoList = list.map((todo) => {
+  state = {
+    addForm:{
+          text:''
+           },
+
+            list : [
+            {
+              "text" : " buy stuff"
+            },
+            {
+              "text" : " buy stuff"
+            },
+            {
+              "text" : " buy stuff"
+            }
+          ]
+  }
+  updateForm = (event) => {
+    const newTask = event.target.value
+    const originalState = this.state.addForm
+    const copy = Object.assign({}, originalState);
+    const key = event.target.name
+    copy[key] = newTask
+    this.setState({
+      addForm: copy
+    })
+  }   
+
+submitTask = (event) => {
+  event.preventDefault()
+  const copy = this.state.list.slice(0)
+  copy.push(this.state.addForm)
+  this.setState({
+    list : copy,
+    addForm: {
+      text:''
+    }
+  })
+}
+
+  render() {
+    
+
+    const todoList = this.state.list.map((todo) => {
       return <Todo todo = {todo} />
     })
     return (
      <div>
+       <form onSubmit={this.submitTask}>
+       <label>text</label>
+       <input type='text' onfocus="this.value=''" name='text' onChange={this.updateForm} value={this.state.addForm.text} />
+       </form>
 {todoList}
       </div>
     );
