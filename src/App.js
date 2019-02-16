5f5832657210fb8728bf242ec22c3196a4eb1a7e
Mhammad1704 <natgeo1416@hotmail.com>
2019-02-16 22:50:11 +0300

    startUp| the map working

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-
+import Todo from './todo.js'
 class App extends Component {
   render() {
+    const list = [
+      {
+        "title": "go out",
+        "text" : " buy stuff"
+      },
+      {
+        "title": "go out",
+        "text" : " buy stuff"
+      },
+      {
+        "title": "go out",
+        "text" : " buy stuff"
+      }
+    ]
+
+    const todoList = list.map((todo) => {
+      return <Todo todo = {todo} />
+    })
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+     <div>
+{todoList}
       </div>
     );
   }
