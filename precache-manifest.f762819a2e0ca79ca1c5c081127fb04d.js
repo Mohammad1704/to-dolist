989c9b87a437513eae538d7600ec621a6cbf9f17
Mhammad1704 <natgeo1416@hotmail.com>
2019-02-20 16:06:08 +0300

    Updates

---
+++ precache-manifest.f762819a2e0ca79ca1c5c081127fb04d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "55db0428aee9e8b3703c",
+    "url": "/to-dolist/static/css/main.f4785653.chunk.css"
+  },
+  {
+    "revision": "55db0428aee9e8b3703c",
+    "url": "/to-dolist/static/js/main.55db0428.chunk.js"
+  },
+  {
+    "revision": "00fe64b82dca7d0213b3",
+    "url": "/to-dolist/static/js/runtime~main.00fe64b8.js"
+  },
+  {
+    "revision": "b2f5ab1217bac76f1d55",
+    "url": "/to-dolist/static/js/2.b2f5ab12.chunk.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/to-dolist/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "f8bf64ba554b8495a100c89c47e755d0",
+    "url": "/to-dolist/index.html"
+  }
+];
