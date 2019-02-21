fdcf23b864ada454d1bfe1f6feb1be3a7f387269
Mhammad1704 <natgeo1416@hotmail.com>
2019-02-21 17:55:40 +0300

    Updates

---
+++ precache-manifest.f55b495595a2915db3e318b1af7a8cf6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "ea15e26ae6ed7cfb2fde",
+    "url": "/react-gh-pages/static/css/main.d3634e30.chunk.css"
+  },
+  {
+    "revision": "ea15e26ae6ed7cfb2fde",
+    "url": "/react-gh-pages/static/js/main.ea15e26a.chunk.js"
+  },
+  {
+    "revision": "cde22863ffca018955eb",
+    "url": "/react-gh-pages/static/js/runtime~main.cde22863.js"
+  },
+  {
+    "revision": "b2f5ab1217bac76f1d55",
+    "url": "/react-gh-pages/static/js/2.b2f5ab12.chunk.js"
+  },
+  {
+    "revision": "e5d83a39707f6a6fe69150c528e664c2",
+    "url": "/react-gh-pages/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/to-dolist/precache-manifest.f762819a2e0ca79ca1c5c081127fb04d.js"
+  "/react-gh-pages/precache-manifest.f55b495595a2915db3e318b1af7a8cf6.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/to-dolist/index.html", {
+workbox.routing.registerNavigationRoute("/react-gh-pages/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
